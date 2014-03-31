309f54565def38c2067be523232fa018eb646be2
German <Ravenwolf2@users.noreply.github.com>
2014-03-31 03:50:30 -0300

    Agregado constraint NOT NULL a dni. Mejorado rakefile

--- db/migrations/001_primera_migracion.rb
+++ db/migrations/001_primera_migracion.rb
@@ -2,7 +2,7 @@ Sequel.migration do
 	change do
 		create_table(:pacientes) do
 			primary_key :id
-			Integer 		:dni, 		 :unique=>true
+			Integer 		:dni, 		 :null=>false, :unique=>true
 			String  		:nombre,   :null=>false
 			String  		:apellido, :null=>false
 		end
